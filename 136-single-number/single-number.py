40f4ceb5e9c9ff26f25159d40bde6dadaca47a04
Anthony Hoang <hoangtrungnguyen18102000@gmail.com>
2025-06-03 10:18:17 +0700

    Time: 0 ms (100.00%) | Memory: 19.6 MB (77.81%) - LeetSync

--- 136-single-number/single-number.py
+++ 136-single-number/single-number.py
@@ -1,17 +1,8 @@
 class Solution:
     def singleNumber(self, nums: List[int]) -> int:
-        counter = {}
-        for e in nums:
-            if counter.get(e) is None:
-                counter[e] = 0
-            else:
-                counter[e] = 1
-        
-        key_found = None # To store the first key found
-        for key, value in counter.items():
-            if value == 0:
-                key_found = key
-                break # Stop searching once the first match is found
-        return key_found
+        single = 0
+        for num in nums:
+            single = single ^ num
+        return single
 
         
