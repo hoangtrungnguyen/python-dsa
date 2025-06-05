58be799b92acad2ca6007f58ab4b544894513f9c
nguyen.hoang <toai.levan@ohmidasvn.com>
2025-06-05 11:37:27 +0700

    snowflakes problem v1

--- hash-table/snowflakes.c
+++ hash-table/snowflakes.c
@@ -1,4 +1,7 @@
+#include <cstdio>
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 #define SIZE 100000
 
 
@@ -48,32 +51,70 @@ int are_identical(int snow1[], int snow2[]){
     return 0;
 }
 
-void identify_identical(int values[][6], int n){
-    int i, j;
-    for( i = 0; i < n; i ++){
-        for( j = i + 1; j < n; j ++){
-            if(are_identical(values[i], values[j])){
-                printf("Twin snowflakes fond..\n");
-                return;
+
+
+int code(int snowflake[]){
+    return (snowflake[0] + snowflake[1] + snowflake[2] + snowflake[3] + snowflake[4] + snowflake[5]) % SIZE;
+}
+
+typedef struct snowflake_node {
+    int snowflake[6];
+    struct snowflake_node *next;
+} snowflake_node;
+
+
+
+
+void identify_identical(snowflake_node *snowflakes[]){
+    snowflake_node  *node1, *node2;
+    int i;
+    for(i =0; i < SIZE; i ++){
+        node1 = snowflakes[i];
+        while(node1 != NULL){
+            node2 = node1->next;
+            while(node2 != NULL){
+                if(are_identical(node1->snowflake, node2 -> snowflake)){
+                    printf("Twin snowflakes found.\n");
+                    return;
+                }
+                node2 = node2 -> next;
             }
+
+            node1 = node1->next;
         }
     }
 
-            printf("No two integers are alike.\n");
+    printf("No two integers are alike.\n");
 }
 
 int main(void) {
-    // int a[5] = {1, 2, 3, 1, 5};
-    // identify_identical(a, 5);
-    // return 0;
-    static int snowflakes[SIZE][6];
-    int n,i,j;
+    //region  create codes
+    static snowflake_node  *snowflakes[SIZE] = {NULL};// one dimensional array of snowflake_node with SIZE
+    snowflake_node *snow;
+
+    int n,i,j, snowflake_code;
     scanf("%d", &n);
     for(i = 0; i < n; i ++){
-        for(j = 0; j < 6; j ++){
-            scanf("%d", &snowflakes[i][j]);
+        snow = malloc(sizeof(snowflake_node));
+        if( snow == NULL){
+            fprintf(stderr, "malloc error\n");
+            exit(1);
         }
+
+        for(j = 0; j < 6; j ++){
+            scanf("%d", &snow->snowflake[j]);
+        }
+        //generate code
+        snowflake_code = code(snow -> snowflake);
+
+        //assign new next node to position: snowflake_code
+        snow->next = snowflakes[snowflake_code];
+        snowflakes[snowflake_code] = snow;
     }
-    identify_identical(snowflakes, n);
+
+    //endregion
+    identify_identical(snowflakes);
+
+
     return 0;
 }
