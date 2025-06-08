48fb1a6140f746db5bc97e863e37d667273cbb59
Anthony Hoang <hoangtrungnguyen18102000@gmail.com>
2025-06-08 14:47:42 +0700

    Time: 0 ms (100.00%) | Memory: 17.7 MB (88.69%) - LeetSync

--- 54-spiral-matrix/spiral-matrix.py
+++ 54-spiral-matrix/spiral-matrix.py
@@ -1,114 +1,44 @@
-class Cell:
-    def __init__(self, x, y, direction: int):
-        self.x = x
-        self.y = y
-        self.direction = direction
-
-
-    def move(self):
-        # 0 -> right
-        # 1 -> bottom
-        # 2 -> left
-        # 3 -> top
-
-        if self.direction == 0:
-            self.y = self.y + 1
-        elif self.direction == 1:
-            self.x = self.x + 1
-        elif self.direction == 2:
-            self.y = self.y - 1
-        elif self.direction == 3:
-            self.x = self.x - 1 
-        else:
-            print("can't find direction")
-
 class Solution:
     def spiralOrder(self, matrix: List[List[int]]) -> List[int]:
         '''
         action = [right, bottom, left, up]
         '''
-        row_length = len(matrix[0])
-        column_length = len(matrix)
-        filled_matrix = [ [0 for j in range(len(matrix[i]))] for i in range(len(matrix))]
+        num_of_row = len(matrix)
+        num_of_col = len(matrix[0])
 
-        def is_right_valid(i:int, j:int):
-            # boundary
-            if j + 1 >= row_length:
-                return False
-            
-            if filled_matrix[i][j + 1] != 0:
-                return False
+        top_row, bottom_row = 0, num_of_row - 1
+        left_col, right_col = 0, num_of_col - 1
 
-            return True
-        
-        def is_bottom_valid(i:int, j:int):
-            # boundary
-            if i + 1 >= column_length:
-                return False
-            
-            if filled_matrix[i + 1][j] != 0:
-                return False
-
-            return True
-
-        def is_left_valid(i:int, j:int):
-            # boundary
-            if j - 1 < 0:
-                return False
-            
-            if filled_matrix[i][j - 1] != 0:
-                return False
-
-            return True
-
-        def is_top_valid(i:int, j:int):
-            # boundary
-            if i - 1 < 0:
-                return False
-            
-            if filled_matrix[i - 1][j] != 0:
-                return False
-
-            return True
-        
-        # def select_direction(current_direction:int):
-        direction_to_validation  = {
-            0: is_right_valid,
-            1: is_bottom_valid,
-            2: is_left_valid,
-            3: is_top_valid
-        }
-
-        # print(direction_to_validation[0](0,0))
-          
-        curr = Cell(0,0,0)
+        # use filled matrix to check
+        # filled_matrix = [ [0 for j in range(len(matrix[i]))] for i in range(len(matrix))]
         result = []
-        
-        # 0 -> right
-        # 1 -> bottom
-        # 2 -> left
-        # 3 -> top
-        
-        while curr != None:
-            filled_matrix[curr.x][curr.y] = 1
-            # print("---")
-            # print(filled_matrix)
-            result.append(matrix[curr.x][curr.y])
-            # prioritize direction
-            # validate from current direction with clock direction
-            current_direction = curr.direction % 4
-            if direction_to_validation[current_direction % 4](curr.x, curr.y):
-                curr.direction = current_direction % 4
-            elif direction_to_validation[(current_direction + 1) % 4 ](curr.x, curr.y):
-                curr.direction = (current_direction + 1) % 4
-            elif direction_to_validation[(current_direction + 2) % 4 ](curr.x, curr.y):
-                curr.direction = (current_direction + 2) % 4
-            elif direction_to_validation[(current_direction + 3) % 4 ](curr.x, curr.y):
-                curr.direction = (current_direction + 3) % 4
-            else:
-                curr = None     
-                break
-            curr.move()
 
+
+        while top_row <= bottom_row and left_col <= right_col:
+            for col in range(left_col, right_col + 1):
+                # filled_matrix[top_row][col] += 1
+                result.append(matrix[top_row][col])
+            top_row += 1 
+            for row in range(top_row, bottom_row + 1):
+                # filled_matrix[row][right_col] += 1
+                result.append(matrix[row][right_col])
+            right_col -= 1
+
+            # print(f'bottom_row {bottom_row}')
+            if top_row <= bottom_row:
+                for col in range(right_col, left_col - 1, -1):
+                    # print(f'col {col}')
+                    # filled_matrix[bottom_row][col] += 1    
+                    result.append(matrix[bottom_row][col])
+                bottom_row -= 1
+            # range will be stop before (exclusive) the value we put in. Therefore, we will have to minus 1
+            # symbol [, )
+            if left_col <= right_col:
+                for row in range(bottom_row, top_row - 1, -1):
+                    # filled_matrix[row][left_col] += 1
+                    result.append(matrix[row][left_col])                
+                left_col += 1
+
+
+        # print(filled_matrix)
         return result
-
