a4364010e1589c3e1907d502e47a3e60b6647304
Anthony Hoang <hoangtrungnguyen18102000@gmail.com>
2025-06-15 16:44:06 +0700

    Time: 0 ms (100.00%) | Memory: 17.9 MB (23.86%) - LeetSync

---
+++ 257-binary-tree-paths/binary-tree-paths.py
@@ -0,0 +1,46 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def binaryTreePaths(self, root: Optional[TreeNode]) -> List[str]:
+        # path = ""
+        result = []
+
+        def dfs(node:Optional[TreeNode], path: str):
+            if node is None:
+                result.append(path)
+                return 
+            print(node.val)
+            # path = f""
+            # result.append(path)
+            # if node.left is None and node.right is None:
+            #     path = ""
+            # print(f"path {path}")
+
+            
+            if path == '':
+                path = f'{node.val}'
+            else:
+                path = f'{path}->{node.val}'
+
+            if node.left is not None:
+                if dfs(node.left, path) is not None:
+                    path = f"{dfs(node.left, path)}->{path}"
+                
+            if node.right is not None:
+                if dfs(node.right, path) is not None:
+                    path = f"{dfs(node.right, path)}->{path}"
+
+            # print(f'path: {path}')
+            if node.left is None and node.right is None:
+                # print(f"path at leaf: {path}")
+                result.append(path)
+
+                # path = f'{node.val}'
+
+        dfs(root, "")
+        return result
+        
