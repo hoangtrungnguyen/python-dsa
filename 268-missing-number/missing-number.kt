e105c736c444230a26d300a9e54400e6871bce21
Anthony Hoang <hoangtrungnguyen18102000@gmail.com>
2025-09-21 13:17:32 +0700

    Time: 0 ms (100.00%) | Memory: 49 MB (55.09%) - LeetSync

---
+++ 268-missing-number/missing-number.kt
@@ -0,0 +1,39 @@
+class Solution {
+    fun missingNumber(nums: IntArray): Int {
+        val l = nums.size
+        var expectedSum = ( l * (l+1)) / 2
+        for(i in 0 until l){
+            expectedSum -= nums[i]
+        }
+        return expectedSum
+
+        for( i in 0 until l) {
+            val tmp = nums[i]
+            if( tmp < l && tmp >= 0){
+                nums[i] = nums[tmp]
+                nums[tmp] = tmp
+            } else {
+                nums[i] = -1
+            }
+                    println(nums.joinToString(", "))
+
+        }
+        for( i in 0 until l){
+            if(nums[i] < 0 || nums[i] > l){
+                return i           
+            }
+        } 
+        return l 
+
+    }
+}
+
+// l = nums.length
+// for i in 0 until n:
+//   value = nums[i]
+//   if (l != value)
+//.     nums[i] = i
+//      nums[value] = value
+//   else:
+//      nums[i] = -1
+// 
