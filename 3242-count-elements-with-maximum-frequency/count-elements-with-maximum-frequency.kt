b042c2abceff9b0d1977a6570316c4ddadc54641
Anthony Hoang <hoangtrungnguyen18102000@gmail.com>
2025-09-22 21:52:40 +0700

    Time: 4 ms (67.74%) | Memory: 44 MB (29.03%) - LeetSync

--- 3242-count-elements-with-maximum-frequency/count-elements-with-maximum-frequency.kt
+++ 3242-count-elements-with-maximum-frequency/count-elements-with-maximum-frequency.kt
@@ -3,24 +3,19 @@ class Solution {
     fun maxFrequencyElements(nums: IntArray): Int {
         val frequencies = mutableMapOf<Int, Int>()
         var maxCount = 0
-        var listOfKeys = mutableListOf<Int>()
+        var total = 0
         for(i in nums.indices){
             val value = nums[i]
             frequencies.putIfAbsent(value, 0)
             frequencies[value] = frequencies[value]!! + 1
             if(frequencies[value]!! > maxCount){
                 maxCount = frequencies[value]!!
-                listOfKeys.clear()
-                listOfKeys.add(value)
+                total = frequencies[value]!!
             } else if (frequencies[value] == maxCount) {
-                listOfKeys.add(value)
+                total += frequencies[value]!!
             }
         }
 
-        var total = 0
-        for(key in listOfKeys){
-            total += frequencies[key]!!
-        }
 
         return total
     }
