3a55154d2af3e1e513a0efdd2f2c25fcb6cabde8
Anthony Hoang <hoangtrungnguyen18102000@gmail.com>
2025-09-23 11:27:26 +0700

    Time: 2 ms (82.81%) | Memory: 41.2 MB (84.38%) - LeetSync

--- 165-compare-version-numbers/compare-version-numbers.kt
+++ 165-compare-version-numbers/compare-version-numbers.kt
@@ -1,45 +1,38 @@
-import kotlin.math.max
 class Solution {
     fun compareVersion(version1: String, version2: String): Int {
-        var data1 = version1.split(".").map<String, Int>{
-//            it.removeLeadingZero
-            it.toInt()
-        }
-        var data2 = version2.split(".").map<String, Int>{
-            it.toInt()
-        }
+        var p1 = 0
+        var p2 = 0
 
-        if(data1.first() > data2.first()){
-            return 1
-        } else if(data1.first() < data2.first()){
-            return -1
-        } else {
-            data1.drop(1)
-            data2.drop(1)
-            println("data1 ${data1}")
-            println("data2 ${data2}")
-            val len = max(data1.size, data2.size)
-            for(i in 0 until len){
-                if (i < data1.size && i < data2.size){
-                    if(data1[i] > data2[i]){
-                        return 1
-                    } else if (data1[i] < data2[i]){
-                        return -1
-                    } else {
-                        continue
-                    }
-                } else if(i > data1.size - 1) {
-                    if(data2[i] > 0){
-                        return -1
-                    }
-                } else if (i > data2.size - 1){
-                    if(data1[i] > 0){
-                        return 1
-                    }
-                }
+        while (p1 < version1.length || p2 < version2.length) {
+            val (num1, nextP1) = getNextChunk(version1, p1)
+            val (num2, nextP2) = getNextChunk(version2, p2)
+            
+            if (num1 < num2) {
+                return -1
+            } else if (num1 > num2) {
+                return 1
             }
-
-            return 0
+            
+            p1 = nextP1
+            p2 = nextP2
         }
+
+        return 0
+    }
+
+    private fun getNextChunk(version: String, p: Int): Pair<Int, Int> {
+        if (p >= version.length) {
+            return Pair(0, p)
+        }
+
+        var end = p
+        while (end < version.length && version[end] != '.') {
+            end += 1
+        }
+
+        val num = if (p < end) version.substring(p, end).toInt() else 0
+        val nextP = end + 1
+
+        return Pair(num, nextP)
     }
 }
