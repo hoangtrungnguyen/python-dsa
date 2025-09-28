29cdbfc533136ed153b0834e0db242de2161d6c4
Anthony Hoang <hoangtrungnguyen18102000@gmail.com>
2025-09-28 22:46:58 +0700

    Time: 18 ms (90.91%) | Memory: 50.1 MB (77.27%) - LeetSync

--- 1018-largest-perimeter-triangle/largest-perimeter-triangle.kt
+++ 1018-largest-perimeter-triangle/largest-perimeter-triangle.kt
@@ -3,11 +3,8 @@ class Solution {
         nums.sort()
         // println("nums ${nums.joinToString(",")}")
         for(i in nums.size - 1 downTo 2){
-            val a = nums[i]
-            val b = nums[i - 1]
-            val c = nums[i - 2]
-            if(b + c > a){
-                return a + b + c
+            if(nums[i - 1] + nums[i - 2]> nums[i]){
+                return nums[i-1] + nums[i-2] + nums[i]
             }
         }
 
