410bf72940b543d40ad40ba218f92564a931ce1a
Anthony Hoang <hoangtrungnguyen18102000@gmail.com>
2025-09-30 12:39:47 +0700

    Time: 4 ms (86.55%) | Memory: 45.9 MB (68.83%) - LeetSync

--- 12-integer-to-roman/integer-to-roman.kt
+++ 12-integer-to-roman/integer-to-roman.kt
@@ -1,35 +1,19 @@
 class Solution {
     fun intToRoman(num: Int): String {
-        var number = num + 1
-        val romanTable = sortedMapOf<Int, String>(
-            1 to "I",
-            4 to "IV",
-            5 to "V",
-            9 to "IX",
-            10 to "X",
-            40 to "XL",
-            50 to "L",
-            90 to "XC",
-            100 to "C",
-            400 to "CD",
-            500 to "D",
-            900 to "CM",
-            1000 to "M",
-        )
+     val values = arrayOf(1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1)
+    val symbols = arrayOf("M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I")
 
-        var roman = StringBuilder()
+    var number = num
+    val roman = StringBuilder()
 
-//        println("Keys: ${romanTable.keys.reversed()}")
-        for(key in romanTable.keys.reversed()) {
-            if(number <= 0){
-                break
-            }
-            while(number > key && number > 0){
-                roman.append(romanTable[key])
-                number -= key
-            }
+    for (i in values.indices) { // We loop through the indices
+        // While the number is large enough...
+        while (number >= values[i]) {
+            // ...we append the symbol from the same index.
+            roman.append(symbols[i])
+            number -= values[i]
         }
-
-        return roman.toString()
+    }
+    return roman.toString()
     }
 }
