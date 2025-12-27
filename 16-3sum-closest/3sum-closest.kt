5dfbbb543a8a3b4733bac9edd82421514c6db3ab
Anthony Hoang <hoangtrungnguyen18102000@gmail.com>
2025-12-27 10:51:46 +0700

    Time: 12 ms (100.00%) | Memory: 45.9 MB (91.30%) - LeetSync

--- 16-3sum-closest/3sum-closest.kt
+++ 16-3sum-closest/3sum-closest.kt
@@ -6,16 +6,40 @@ class Solution {
         // Initialize the closest sum with the first possible triplet
         var closestSum = nums[0] + nums[1] + nums[2]
 
-        var distance = -1
-
         // Iterate through the array, using 'anchor' as the first element of the triplet
         for (anchor in 0 until nums.size - 2) {
+            if(anchor > 1 && nums[anchor] == nums[anchor -1]) continue
+
             // Use two pointers for the remaining two elements
             var p1 = anchor + 1
+
+            // check smallest possible sum
+            val smallestSum = nums[anchor] + nums[p1] + nums[p1 + 1]
+            // if smallestSum > target. Next sum will only be bigger.
+            // Then, we need to check with previous closest sum (when anchor < anchor - 1).
+            // We choose between current smallestSum with previous Sum
+            if( smallestSum > target){
+                if(Math.abs(smallestSum - target) < Math.abs(closestSum - target)){
+                    closestSum = smallestSum
+                }
+                break
+            }
+
             var p2 = nums.size - 1
 
+            // check biggest possible sum
+            val maxSum = nums[anchor] + nums[p2] + nums[p2 - 1]
+
+            if(maxSum < target){
+                if(Math.abs(maxSum - target) < Math.abs(closestSum - target)){
+                    closestSum = maxSum
+                }
+                continue
+            }
+
+        
             while (p1 < p2) {
-                val currentSum = sum3(nums, anchor, p1, p2)
+                val currentSum = nums[anchor] + nums[p1] + nums[p2]
                 
                 // If the exact target is found, return immediately
                 if (currentSum == target) {
