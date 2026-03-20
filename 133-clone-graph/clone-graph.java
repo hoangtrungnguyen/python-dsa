50f445c1966256522e30bd8dd09874208d342b22
Anthony Hoang <hoangtrungnguyen18102000@gmail.com>
2026-03-20 14:54:06 +0700

    Time: 23 ms (99.24%) | Memory: 44.2 MB (85.00%) - LeetSync

---
+++ 133-clone-graph/clone-graph.java
@@ -0,0 +1,60 @@
+/*
+// Definition for a Node.
+class Node {
+    public int val;
+    public List<Node> neighbors;
+    public Node() {
+        val = 0;
+        neighbors = new ArrayList<Node>();
+    }
+    public Node(int _val) {
+        val = _val;
+        neighbors = new ArrayList<Node>();
+    }
+    public Node(int _val, ArrayList<Node> _neighbors) {
+        val = _val;
+        neighbors = _neighbors;
+    }
+}
+*/
+
+class Solution {
+    public Node cloneGraph(Node node) {
+        Queue<Node> queue = new LinkedList<>();
+        // choseNode = node
+        // map = {}
+        // queue = []
+        // 1. creat new Node. map[choseNode] = newNode
+        // 2. loop through each child in a chosen node. 
+        //      1. map[child] = child
+        //      2. queue.enque(child)
+        // 4. 
+        // 5. 
+        Map<Integer, Node> memory = new HashMap<>();
+
+        return clone(memory, node);
+    }
+
+
+    private Node clone(Map<Integer, Node> map, Node node){
+        if(node == null){
+            return null;
+        }
+        Node chosen;
+        if(map.containsKey(node.val)){
+            chosen = map.get(node.val);
+            return chosen;
+        } else {
+            chosen = new Node(node.val);
+            map.put(node.val, chosen);
+        }
+
+
+        for(Node child : node.neighbors){
+            Node clonedChild = clone(map, child);
+            chosen.neighbors.add(clonedChild);
+        }
+
+        return chosen;
+    }
+}
