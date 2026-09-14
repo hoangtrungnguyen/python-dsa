592733cc0e2f00c429e3db262c1e9f91022ca6e7
Anthony Hoang <hoangtrungnguyen18102000@gmail.com>
2026-09-14 10:55:09 +0700

    Time: 1 ms (99.97%) | Memory: 43.1 MB (98.08%) - LeetSync

--- 67-add-binary/add-binary.java
+++ 67-add-binary/add-binary.java
@@ -1,31 +1,15 @@
 class Solution {
-    public String addBinary(String input1, String input2) {
-        String a, b;
-        if (input1.length() > input2.length()) { a = input1; b = input2; }
-        else { a = input2; b = input1; }
-
-        int[] bitsA = a.chars().map(c -> c - '0').toArray();
-        int[] bitsB = b.chars().map(c -> c - '0').toArray();
-
-        int length = bitsA.length;
-        int diff = bitsA.length - bitsB.length;
-
-        int[] result = new int[length + 1];
-        int carry = 0;
-
-        for (int i = 0; i < length; i++) {
-            int aIndex = length - i - 1;
-            int bIndex = aIndex - diff;
-            int cur = bitsA[aIndex] + (bIndex >= 0 ? bitsB[bIndex] : 0) + carry;
-            result[aIndex + 1] = cur % 2;
-            carry = cur / 2;
-        }
-        result[0] = carry;
-
+    public String addBinary(String a, String b) {
         StringBuilder sb = new StringBuilder();
-        for (int bit : result) sb.append(bit);
-        int start = 0;
-        while (start < sb.length() - 1 && sb.charAt(start) == '0') start++;
-        return sb.substring(start);
+        int i = a.length() - 1, j = b.length() - 1, carry = 0;
+
+        while (i >= 0 || j >= 0 || carry > 0) {
+            int sum = carry;
+            if (i >= 0) sum += a.charAt(i--) - '0';
+            if (j >= 0) sum += b.charAt(j--) - '0';
+            sb.append(sum % 2);
+            carry = sum / 2;
+        }
+        return sb.reverse().toString();
     }
 }
